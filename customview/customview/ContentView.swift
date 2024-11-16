4dd0cd112645382f2d32bceaa05cb2d8487caae3
huntvee <telotest@gmail.com>
2024-11-16 16:18:52 +0100

    edited forecast app

--- customview/customview/ContentView.swift
+++ customview/customview/ContentView.swift
@@ -9,15 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            
-            DayForecast(day: "Mon", high: 70, low: 50, isRainy: false)
-            
-            DayForecast(day: "Tue", high: 30, low: 20, isRainy: true)
-            
-            DayForecast(day: "Wed", high: 20, low:15, isRainy: true)
-            
+        ScrollView{
+            VStack (spacing:25){
+                HStack {
+                    
+                    DayForecast(day: "Mon", high: 70, low: 50, isRainy: false)
+                    
+                    DayForecast(day: "Tue", high: 30, low: 20, isRainy: true)
+                    
+                    DayForecast(day: "Wed", high: 20, low:15, isRainy: true)
+                    
+                    }
+                HStack {
+                    
+                    DayForecast(day: "Mon", high: 70, low: 50, isRainy: false)
+                    
+                    DayForecast(day: "Tue", high: 30, low: 20, isRainy: true)
+                    
+                    DayForecast(day: "Wed", high: 20, low:15, isRainy: true)
+                    
+                    }
             }
+            
+        }
             .padding()
         }
     }
@@ -45,18 +59,41 @@ struct DayForecast: View {
             return Color.yellow
         }
     }
+    var hightempColor: Color {
+        if high >= 60 {
+            return Color.red
+        } else {
+            return Color.black
+        }
+    }
+    var lowtempColor: Color {
+        if low <= 15 {
+            return Color.blue
+        } else {
+            return Color.black
+        }
+    }
     var body: some View {
         VStack {
             Text(day)
                 .font(.title)
             Image(systemName: iconName)
                 .foregroundStyle(iconColor)
+                .font(.largeTitle)
+                .padding(5)
             Text("High: \(high)")
                 .font(.headline)
+                .foregroundStyle(hightempColor)
             Text("Low: \(low)")
                 .font(.headline)
+                .foregroundStyle(lowtempColor)
             
             
-    }
+            }
         }
     }
+struct WeekForecast: View{
+    var body: some View {
+        Text("hi")
+    }
+}
